660bf99a966758e09013ead64e5462fcfe5e1495
“singhprashant1” <ps9956651@gmail.com>
2021-01-10 23:44:59 +0530

    newupdate

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:socialm/homepage.dart';
@@ -7,7 +8,7 @@ import 'constent.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp();
   Constants.prefs = await SharedPreferences.getInstance();
 
   runApp(MaterialApp(
