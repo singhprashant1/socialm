168563d3af70ac307edd15b2d76e673cd70089ff
“singhprashant1” <ps9956651@gmail.com>
2021-01-14 00:51:23 +0530

    newupdate

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:socialm/constent.dart';
 import 'package:socialm/homepage.dart';
@@ -193,7 +194,7 @@ class _LoginPageState extends State<LoginPage> {
       try {
         UserCredential user = await auth.signInWithEmailAndPassword(
             email: emailController.text, password: passController.text);
-
+        user.user;
         Constants.prefs.setBool("loggedIn", true);
         Navigator.push(
             context, MaterialPageRoute(builder: (context) => ProfilePage()));
