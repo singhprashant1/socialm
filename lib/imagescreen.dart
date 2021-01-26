12b47bca3e31491caebb6371b71eeda56c78ea8b
“singhprashant1” <ps9956651@gmail.com>
2021-01-27 03:29:14 +0530

    newupdate

--- lib/imagescreen.dart
+++ lib/imagescreen.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ImageScreen extends StatefulWidget {
   final number;
@@ -10,11 +14,49 @@ class ImageScreen extends StatefulWidget {
 class _ImageScreenState extends State<ImageScreen> {
   _ImageScreenState(this.number);
   String number;
+  String username;
+  var link;
+  void readData() async {
+    final db = FirebaseDatabase.instance.reference().child("Cust");
+    User user = FirebaseAuth.instance.currentUser;
+    db
+        .orderByChild("uid")
+        .equalTo(user.uid)
+        .once()
+        .then((DataSnapshot snapshot) {
+      snapshot.value.forEach((key, values) {
+        setState(() {
+          username = values["username"];
+
+          print(username);
+        });
+        return username;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Firebase.initializeApp().whenComplete(() {
+      print("completed");
+      readData();
+      // datee();
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("HEy"),
+        backgroundColor: Colors.white,
+        title: Text(
+          username != null ? "" + username.toString() : "loading",
+          style: GoogleFonts.montserrat(
+            textStyle: TextStyle(color: Colors.black, fontSize: 22),
+          ),
+        ),
       ),
       body: Stack(
         fit: StackFit.expand,
